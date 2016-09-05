a754ccd58af8aab32348e5f418fba6b1d878266f
Edward Kelly <ed7kelly@gmail.com>
2016-09-06 11:01:10 +1200

    Partly implemented Movable

--- Missing/src/missing/game/items/Movable.java
+++ Missing/src/missing/game/items/Movable.java
@@ -1,8 +1,27 @@
+/* File: Movable.java
+ * 
+ * Authors:
+ * Edward Kelly 300334192
+ * 
+ * Date			Author			Modification
+ * 5 Sep 16		Edward Kelly	created Movable class
+ */
 package missing.game.items;
+
+import java.awt.Point;
+
 /**
- * Represents an item that can be moved/carried in the game
+ * Represents an item that can be moved/carried in the game. Movable items 
+ * can have their location changed.
  *
  */
 public abstract class Movable extends Item {
-
+	
+	public void setWorldLocation(Point destination){
+		super.worldLocation = destination;
+	}
+	
+	public void setRenderLocation(Point destination){
+		super.renderLocation = destination;
+	}
 }
