c39f6593449b34bc02abe170bcbc97d0417c2dc9
veryasian <linussgo@gmail.com>
2016-09-06 12:13:40 +1200

    Updated the NonMovable class. Removed the Placeholder class.

--- Missing/src/missing/game/items/NonMovable.java
+++ Missing/src/missing/game/items/NonMovable.java
@@ -2,11 +2,15 @@
  * 	
  * 	Authors:			ID
  * 	Chris Rabe			300334207
- * 
+ *  Linus Go			300345571
  * 	Date				Author					Changes
  * 	5 Sep 16			Chris Rabe				added java documentation		
+ *  6 Sep 16			Linus Go				updated documentation and added methods.
+ *  6 Sep 16			Linus Go				Added an abstract display method.
  */
 package missing.game.items;
+
+import java.awt.Graphics;
 
 /**
  * This class provides skeleton implementation of items which cannot be moved by
@@ -15,9 +19,18 @@ package missing.game.items;
  * player.
  */
 public abstract class NonMovable extends Item {
-
+	/**
+	 * Constructs an instance of a NonMovable object.
+	 * @param name
+	 * @param description
+	 */
 	public NonMovable(String name, String description) {
 		super(name, description);
 	}
+	
+	/**
+	 * Draws the current Non-movable item onto the screen.
+	 */
+	public abstract void display(Graphics g);
 
 }
