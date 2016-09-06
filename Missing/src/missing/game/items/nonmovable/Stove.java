7021b7d9ca61b6ba2c61258fb51954e4ef0fd8e7
oznaprazzi <oznaprazzi@OZNAPRAZZI>
2016-09-06 22:18:03 +1200

    Added the log comments on the top of the classes

--- Missing/src/missing/game/items/nonmovable/Stove.java
+++ Missing/src/missing/game/items/nonmovable/Stove.java
@@ -1,3 +1,12 @@
+/* File: Stove.java
+ * 
+ * Authors			ID
+ * Casey Huang		300316284
+ * 
+ * Date				Author			Modification
+ *	6 Sep 2016		Casey Huang		Created Stove abstract class
+ *	6 Sep 2016		Casey Huang		Added javadoc comments and descriptions.
+ */
 package missing.game.items.nonmovable;
 
 import java.awt.Graphics;
@@ -6,9 +15,20 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * Represents a Stove item used for cooking
+ * @author oznaprazzi
+ *
+ */
 public class Stove extends Cooking{
 	private int width;
 	private int height;
+	
+	/**
+	 * Creates an instance of a Stove item.
+	 * @param name
+	 * @param description
+	 */
 	public Stove(String name, String description) {
 		super(name, description);
 	}
@@ -18,7 +38,6 @@ public class Stove extends Cooking{
 		try {
 			g.drawImage(ImageIO.read(new File("stove.png")) , x, y, width, height, null);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
