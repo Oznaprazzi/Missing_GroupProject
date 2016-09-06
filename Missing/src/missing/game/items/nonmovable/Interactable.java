9fcde4e05803991622da0dcded6f177bc75266eb
veryasian <linussgo@gmail.com>
2016-09-06 12:47:11 +1200

    Added NonInteractable and Interactable classes.

---
+++ Missing/src/missing/game/items/nonmovable/Interactable.java
@@ -0,0 +1,32 @@
+/* File: Interactable.java
+ * 
+ * Authors			ID
+ * Linus Go			300345571
+ * 
+ * Date				Author			Modification
+ *	6 Sep 2016		Linus Go		Created Interactable class
+ *	6 Sep 2016		Linus Go		Added javadoc comments and descriptions.
+ */
+package missing.game.items.nonmovable;
+import missing.game.items.NonMovable;
+
+/**
+ * Class that represents an Interactable item. A user can Interact with these type of items,
+ * such as a door, a light switch or any type of non-movable toggle.
+ * @author linus
+ *
+ */
+public abstract class Interactable extends NonMovable{
+	
+	/**
+	 * Create a new Instance of an Interactable item.
+	 * @param name
+	 * @param description
+	 */
+	public Interactable(String name, String description) {
+		super(name, description);
+	}
+
+
+	
+}
---
+++ Missing/src/missing/game/items/nonmovable/NonInteractable.java
@@ -0,0 +1,43 @@
+/* File: NonInteractable.java
+ * 
+ * Authors			ID
+ * Linus Go			300345571
+ * 
+ * Date				Author			Modification
+ *	6 Sep 2016		Linus Go		Created NonInteractable class
+ *	6 Sep 2016		Linus Go		Added javadoc comments and descriptions.
+ *	6 Sep 2016		Linus Go		Added display filler code.
+ */
+
+package missing.game.items.nonmovable;
+import java.awt.Graphics;
+
+import missing.game.items.NonMovable;
+
+/**
+ * Class that represents an Non-Interactable item. The item may be purely decorative and serve no actual purpose in terms of gameplay.
+ * It contains a position and a size.
+ * @author linus
+ *
+ */
+public class NonInteractable extends NonMovable{
+	
+	/**
+	 * Create a new Instance of an Non-Interactable item.
+	 * @param name
+	 * @param description
+	 */
+	public NonInteractable(String name, String description) {
+		super(name, description);
+	}
+	
+	/**
+	 * Draws the current NonInteractable object.
+	 */
+	@Override
+	public void display(Graphics g) {
+		// TODO Auto-generated method stub
+	}
+	
+	
+}
