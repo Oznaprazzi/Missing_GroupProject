72e400f82af89d80ac3e563378f5b01378b9535b
oznaprazzi <oznaprazzi@OZNAPRAZZI>
2016-09-06 22:38:24 +1200

    Added all Furniture related classes and added images for them

--- Missing/src/missing/game/items/nonmovable/Bed.java
+++ Missing/src/missing/game/items/nonmovable/Bed.java
@@ -1,10 +1,10 @@
-/* File: Stove.java
+/* File: Bed.java
  * 
  * Authors			ID
  * Casey Huang		300316284
  * 
  * Date				Author			Modification
- *	6 Sep 2016		Casey Huang		Created Stove class
+ *	6 Sep 2016		Casey Huang		Created Bed class
  *	6 Sep 2016		Casey Huang		Added javadoc comments and descriptions.
  */
 package missing.game.items.nonmovable;
@@ -16,30 +16,29 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 /**
- * Represents a Stove item used for cooking
- * @author oznaprazzi
+ * Represents a Bed that a player can sleep on to gain energy.
+ * @author Casey Huang
  *
  */
-public class Stove extends Cooking{
+public class Bed extends Medical{
 	private int width;
 	private int height;
 	
 	/**
-	 * Creates an instance of a Stove item.
+	 * Creates an instance of a Bed item.
 	 * @param name
 	 * @param description
 	 */
-	public Stove(String name, String description) {
+	public Bed(String name, String description){
 		super(name, description);
 	}
 
 	@Override
 	public void display(Graphics g, int x, int y) {
 		try {
-			g.drawImage(ImageIO.read(new File("stove.png")) , x, y, width, height, null);
+			g.drawImage(ImageIO.read(new File("bed.png")) , x, y, width, height, null);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
