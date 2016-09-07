ef63680ff132770a7f55a39e20de1054c3552f48
veryasian <linussgo@gmail.com>
2016-09-07 12:15:35 +1200

    Added size field to Movable class. Updated hashCode, toString and equals.

--- Missing/src/missing/game/items/movable/Movable.java
+++ Missing/src/missing/game/items/movable/Movable.java
@@ -2,9 +2,11 @@
  * 
  * Authors:
  * Edward Kelly 300334192
+ * Linus Go		300345571
  * 
  * Date			Author			Modification
  * 7 Sep 16		Edward Kelly	created Movable class
+ * 7 Sep 16		Linus Go		Added size field and getters and setters.
  */
 package missing.game.items.movable;
 
@@ -23,6 +25,9 @@ public abstract class Movable extends Item {
 	 * capacity and player movement speed */
 	protected double weight;
 	
+	/**Represents the size of this movable object. **/
+	protected int size;
+	
 	/**
 	 * Creates a new instance of a Movable item
 	 * @param name name of Movable
@@ -31,26 +36,43 @@ public abstract class Movable extends Item {
 	 * @param tileLocation the tile inside a worldLocation the Movable item is located
 	 * @param weight weight of the Movable item
 	 */
-	public Movable(String name, String description, Point worldLocation, Point tileLocation, int weight) {
+	public Movable(String name, String description, Point worldLocation, Point tileLocation, int weight, int size) {
 		super(name, description, worldLocation, tileLocation);
 		this.weight = weight;
+		this.size= size;
 	}
-	
+	/**
+	 * Return the weight of this movable item.
+	 * @return weight - (double)
+	 */
 	public double getWeight(){
 		return weight;
 	}
+	
+	/**
+	 * Return the size of this movable item.
+	 * @return
+	 */
+	public int getSize(){
+		return this.size;
+	}
 
 	// Overridden Object methods
+
+	@Override
+	public String toString(){
+		return super.toString() + " weight: " + weight + " size: " + size;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
+		result = prime * result + size;
 		long temp;
 		temp = Double.doubleToLongBits(weight);
 		result = prime * result + (int) (temp ^ (temp >>> 32));
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -60,13 +82,10 @@ public abstract class Movable extends Item {
 		if (getClass() != obj.getClass())
 			return false;
 		Movable other = (Movable) obj;
+		if (size != other.size)
+			return false;
 		if (Double.doubleToLongBits(weight) != Double.doubleToLongBits(other.weight))
 			return false;
 		return true;
-	}
-	
-	@Override
-	public String toString(){
-		return super.toString() + " weight: " +weight;
 	}
 }
