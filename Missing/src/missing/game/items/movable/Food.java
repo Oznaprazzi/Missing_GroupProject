b3493b3df459bddb2f1aa686656fb385c43f1362
oznaprazzi <oznaprazzi@OZNAPRAZZI>
2016-09-07 19:17:15 +1200

    Updated the food class with enum, helper method and toString method

--- Missing/src/missing/game/items/movable/Food.java
+++ Missing/src/missing/game/items/movable/Food.java
@@ -5,6 +5,9 @@
  * 
  * Date			Author			Modification
  * 7 Sep 16		Linus Go		created Food.java
+ * 7 Sep 16 	Casey Huang		Created the enum for the type of food and created the helper method for changing the name and description
+ * 								of the type of food.
+ * 7 Sep 16 	Casey Huang		Created the toString method.
  */
 package missing.game.items.movable;
 
@@ -14,8 +17,8 @@ import java.awt.Point;
  * This class represents Food Items. Classes that extend the Food class are edible items that can be eaten by players to increase their health.
  *
  */
-public abstract class Food extends Health {
-	
+public class Food extends Health {
+	private FoodType foodType= null;
 	/**
 	 * Create a new instance of a Food object.
 	 * @param name
@@ -30,4 +33,45 @@ public abstract class Food extends Health {
 		super(name, description, worldLocation, tileLocation, amount, size, REGAMOUNT);
 	}
 
+	/**
+	 * Represents the type of food.
+	 */
+	public static enum FoodType{
+		Fruit,
+		Chicken,
+		Beef
+	}
+	
+	/**
+	 * Sets the type of food.
+	 * @param f
+	 */
+	public void setFoodType(FoodType f){
+		foodType = f;
+		this.setInfo();
+	}
+	
+	/**
+	 * Sets the name and description for each type of food.
+	 */
+	public void setInfo(){
+		switch(foodType){
+		case Fruit:
+			name = "Fruit";
+			description = "A tasty piece of fruit";
+			break;
+		case Chicken:
+			name = "Chicken";
+			description = "A tasty piece of chicken";
+			break;
+		case Beef:
+			name = "Beef";
+			description = "A delicious beef meal";
+			break;
+		}
+	}
+	
+	public String toString(){
+		return "Name: " + name + " Description: " + description;
+	}
 }
