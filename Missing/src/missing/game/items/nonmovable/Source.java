7beac7597770ca2ce615accd64a148e6c3b9e3be
oznaprazzi <oznaprazzi@OZNAPRAZZI>
2016-09-07 13:41:39 +1200

    Created Furniture and Source classes and removed author from Interactable and NonInteractable classes.

--- Missing/src/missing/game/items/nonmovable/Source.java
+++ Missing/src/missing/game/items/nonmovable/Source.java
@@ -1,32 +1,29 @@
-/*	File: NonMovable.java
+/*	File: Source.java
  * 	
  * 	Authors:			ID
  *	Casey Huang			300316284
  * 	Date				Author					Changes
- *	7 Sep 16 			Casey Huang				Created Interactable class and added javaDoc comments
- *	7 Sep 16  			Casey Huang				Created performAction method
+ *	7 Sep 16 			Casey Huang				Created Source class and added javaDoc comments
  */
 package missing.game.items.nonmovable;
 
 import java.awt.Point;
-
 /**
- * Represents an interactable item in the game.
+ * Represents a Source item that a player can collect
  *
  */
-public abstract class Interactable extends NonMovable{
-
+public abstract class Source extends Interactable{
 	/**
-	 * Creates an instance of an Interactable item.
+	 * Creates a new instance of a Source item.
 	 * @param name
 	 * @param description
 	 * @param worldLocation
 	 * @param tileLocation
 	 */
-	public Interactable(String name, String description, Point worldLocation, Point tileLocation) {
+	public Source(String name, String description, Point worldLocation, Point tileLocation) {
 		super(name, description, worldLocation, tileLocation);
 	}
-	
+
 	/**
 	 * For player to perform an action on this Interactable item.
 	 */
