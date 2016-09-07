1a3b3aa5035dab0df259a04cd7655c4aa764b8f5
Edward Kelly <ed7kelly@gmail.com>
2016-09-07 14:52:42 +1200

    Added Bush class

---
+++ Missing/src/missing/game/items/nonmovable/Bush.java
@@ -0,0 +1,27 @@
+/*	File: Bush.java
+ * 
+ * 	Author			ID
+ * 	Edward Kelly	300334192
+ * 	
+ * 	Date			Author				Changes
+ * 	7 Sep 16		Edward Kelly		created Bush class
+ */
+package missing.game.items.nonmovable;
+
+import java.awt.Point;
+/**
+ * Represents a Bush. A Bush is only for aesthetics.
+ * Subclass of Foliage
+ */
+public class Bush extends Foliage{
+	
+	/**
+	 * Creates a new Bush object at the given locations
+	 * @param worldLocation section of world Bush is located
+	 * @param tileLocation the tile inside the worldLocation the Bush is located
+	 */
+	public Bush(Point worldLocation, Point tileLocation) {
+		super("Bush", "It's a bush", worldLocation, tileLocation);
+	}
+	
+}
