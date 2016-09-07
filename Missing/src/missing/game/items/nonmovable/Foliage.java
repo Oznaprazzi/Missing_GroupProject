afc5ba5081ecd187100445d72a8ac54e9e296381
Christiandel Rabe <chris.rabe14@gmail.com>
2016-09-07 13:59:32 +1200

    finished implementing foliage class.

--- Missing/src/missing/game/items/nonmovable/Foliage.java
+++ Missing/src/missing/game/items/nonmovable/Foliage.java
@@ -1,5 +1,23 @@
+/*	File: Foliage.java
+ * 
+ * 	Author			ID
+ * 	Chris Rabe		300334207
+ * 	
+ * 	Date			Author				Changes
+ * 	7 Sep 16		Chris Rabe			create foliage class
+ */
+
 package missing.game.items.nonmovable;
 
-public class Foliage {
+import java.awt.Point;
 
+/**
+ * This class represents non movable objects which is purely for aesthetic
+ * reasons.
+ */
+public class Foliage extends NonInteractable {
+
+	public Foliage(String name, String description, Point worldLocation, Point tileLocation) {
+		super(name, description, worldLocation, tileLocation);
+	}
 }
