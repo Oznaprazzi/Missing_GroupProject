8ce5b7f7a4023f5b8022aad87d75a76989409cd3
Christiandel Rabe <chris.rabe14@gmail.com>
2016-09-08 10:53:40 +1200

    create world and worldNode classes.

---
+++ Missing/src/missing/game/world/World.java
@@ -0,0 +1,18 @@
+/*	File: World.java
+ * 	Author:
+ * 	Chris Rabe		300334207
+ * 
+ * 	Date			Author				changes
+ * 	8 Sep 16		Chris Rabe			created world class
+ */
+
+package missing.game.world;
+
+/**
+ * This class contains all the logic for moving around the players within the
+ * game world as well as performing appropriate checking if item and players
+ * collide.
+ */
+public class World {
+
+}
---
+++ Missing/src/missing/game/world/nodes/WorldNode.java
@@ -0,0 +1,17 @@
+/*	File: WorldNode.java
+ * 	Author
+ * 	Chris Rabe		300334207
+ * 	Date			Author				Changes
+ * 	8 Sep 16		Chris Rabe			create worldNode class
+ */
+
+package missing.game.world.nodes;
+
+/**
+ * World node contains various types of world tiles. It should also contain a
+ * list of movable and nonMovable objects because this information will be
+ * needed when managing server.
+ */
+public class WorldNode {
+
+}
