74e5f8ffecc7681e4473bcb2c16c0ca5c7bd1edf
Christiandel Rabe <chris.rabe14@gmail.com>
2016-09-07 13:31:17 +1200

    added amount field in movable class. Updated constructor

--- Missing/src/missing/game/items/movable/Resource.java
+++ Missing/src/missing/game/items/movable/Resource.java
@@ -9,23 +9,17 @@
 package missing.game.items.movable;
 
 import java.awt.Point;
+
 /**
- * This class represents a 
+ * This class represents a
+ * 
  * @author linus
  *
  */
 public abstract class Resource extends Movable {
-	/**
-	 * Construct a new Instance of a Resource.
-	 * @param name
-	 * @param description
-	 * @param worldLocation
-	 * @param tileLocation
-	 * @param weight
-	 * @param size
-	 */
-	public Resource(String name, String description, Point worldLocation, Point tileLocation, int weight, int size) {
-		super(name, description, worldLocation, tileLocation, weight, size);
+
+	public Resource(String name, String description, Point worldLocation, Point tileLocation, int amount, int size) {
+		super(name, description, worldLocation, tileLocation, size, size);
 		// TODO Auto-generated constructor stub
 	}
 
