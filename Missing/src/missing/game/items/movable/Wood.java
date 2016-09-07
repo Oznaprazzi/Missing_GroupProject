a50e1ab882bcd3f721b2c08a746d2e78c76495ea
Christiandel Rabe <chris.rabe14@gmail.com>
2016-09-07 14:38:51 +1200

    edited contructor for Wood

--- Missing/src/missing/game/items/movable/Wood.java
+++ Missing/src/missing/game/items/movable/Wood.java
@@ -18,19 +18,26 @@ import java.awt.Point;
 public class Wood extends Resource {
 	/**
 	 * Construct a new Instance of a Resource.
-	 * @param name name of the item
-	 * @param description describes what the item is
-	 * @param worldLocation the section of the world this item is located in
-	 * @param tileLocation the tile in the worldLocation in which the item is located
-	 * @param size represents size of item
-	 * @param amount amount of Wood items
-	*/
-	public Wood(String name, String description, Point worldLocation, Point tileLocation, int amount, int size) {
-		super(name, description, worldLocation, tileLocation, amount, size);
+	 * 
+	 * @param name
+	 *            name of the item
+	 * @param description
+	 *            describes what the item is
+	 * @param worldLocation
+	 *            the section of the world this item is located in
+	 * @param tileLocation
+	 *            the tile in the worldLocation in which the item is located
+	 * @param size
+	 *            represents size of item
+	 * @param amount
+	 *            amount of Wood items
+	 */
+	public Wood(Point worldLocation, Point tileLocation, int amount, int size) {
+		super("Wood", "Planks of Wood.", worldLocation, tileLocation, amount, size);
 	}
-	
+
 	@Override
-	public String toString(){
+	public String toString() {
 		return "Resource: Wood, " + super.toString();
 	}
 }
