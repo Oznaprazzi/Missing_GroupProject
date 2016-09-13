250878785e64f4bc4f6887ba63d842fef89509aa
veryasian <linussgo@gmail.com>
2016-09-13 12:06:51 +1200

    Added VController class and packages.

---
+++ Missing/src/missing/ui/controller/VControl.java
@@ -0,0 +1,28 @@
+/*
+ * AUTHORS 
+ * Linus Go	300345571
+ * 
+ * Updates : 
+ * 13/9 - Created VControl.java
+ * 
+ */
+package missing.ui.controller;
+import javax.swing.JFrame;
+
+/**
+ * 
+ * This Class represents the Controller for the Game. It extends JFrame, and will contain a View, and buttons and toggles that the user
+ * is able to manipulate.
+ *
+ */
+@SuppressWarnings("serial")
+public class VControl extends JFrame{
+	
+	public VControl(){
+		
+	}
+	
+	
+	
+	
+}
