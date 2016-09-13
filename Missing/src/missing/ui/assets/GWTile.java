406e89ea4a31f4d6cd2747f72353959ffed91153
jianw <jianw@LAPTOP-62IPL4J1>
2016-09-13 12:46:00 +1200

    created the gWorld, GWTile and GWNode classes

---
+++ Missing/src/missing/ui/assets/GWTile.java
@@ -0,0 +1,12 @@
+package missing.ui.assets;
+import missing.game.*;
+import missing.game.world.nodes.WorldTile;
+public class GWTile {
+
+	private WorldTile tile;
+	
+	public GWTile(WorldTile tile){
+		this.tile=tile;
+	}
+	
+}
---
+++ Missing/src/missing/ui/assets/GWorld.java
@@ -0,0 +1,18 @@
+package missing.ui.assets;
+
+import missing.game.world.World;
+
+public class GWorld {
+	
+	private World world;
+	private GWNode[][] GWNodes = new GWNode[World.WORLD_WIDTH][World.WORLD_HEIGHT];
+	public GWorld(World w){
+		world = w;
+		initialiseGWNodes();
+	}
+	
+	public void initialiseGWNodes(){
+		for(int i=0; i<)
+	}
+
+}
