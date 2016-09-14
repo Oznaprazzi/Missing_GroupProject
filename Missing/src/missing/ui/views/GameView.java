a924b34336d071e5d33e5bc473d024e86eb0a504
veryasian <linussgo@gmail.com>
2016-09-15 11:31:39 +1200

    Finalized the Menu, added confirmation dialogs for exitting the game.

---
+++ Missing/src/missing/ui/views/GameView.java
@@ -0,0 +1,34 @@
+package missing.ui.views;
+
+import missing.ui.controller.VControl;
+import missing.ui.controller.VControl.View;
+import javax.swing.JSplitPane;
+import javax.swing.JPanel;
+/**
+ * This class represents the Game View in the regular sate.
+ * @author linus
+ *
+ */
+public class GameView extends View {
+	
+	public GameView(VControl controller) {
+		super(controller);
+	}
+
+	@Override
+	public void initialise() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+	}
+	
+	
+	
+	
+	
+	
+
+}
--- Missing/src/missing/ui/views/MenuView.java
+++ Missing/src/missing/ui/views/MenuView.java
@@ -4,6 +4,7 @@
  * 	
  * 	Date:					Author					Changes
  *	14 Sep 16				Linus Go				Added the MenuView Class.
+ *	15 Sep 16				Linus Go				Added buttons for the MenuView class.
  */
 package missing.ui.views;
 
@@ -12,6 +13,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 import missing.ui.controller.VControl;
 import missing.ui.controller.VControl.View;
@@ -31,6 +33,14 @@ public class MenuView extends View {
 	private JButton btnMultiPlayer;
 	private JButton btnAbout;
 	private JButton btnExit;
+	private final int buttonWd = 250;
+	private final int buttonHt = 61;
+	private final int buttonX = 360;
+	/**The credit message String */
+	private String creditMessage = "\"Missing\" was Created for our SWEN 222 Group Project, and was intended to be an Adventure Game."
+			+ "\nWe had to work with 5 people on this game, and spent many hours and days over this project."
+			+ "\nThe Team: "
+			+ "\n (1) Linus Go\n (2) Edward Kelly\n (3) Jian Wei Chong\n (4) Christiandel Rabe\n (5) Casey Huang";
 
 	/**
 	 * This constructs a new instance of a MenuView.
@@ -39,67 +49,64 @@ public class MenuView extends View {
 	 */
 	public MenuView(VControl controller) {
 		super(controller);
-		this.initialiseLayout();
-	}
-	/**
-	 * This method defines the action listeners and allows actions to be performed once the button is pressed.
-	 */
-	private void setupActionListeners() {
-		btnSinglePlayer.addActionListener(e->{
-			
-		});
-		
-		btnMultiPlayer.addActionListener(e->{
-			
-		});
-		
-		btnAbout.addActionListener(e->{
-			
-		});
-		
-		btnExit.addActionListener(e->{
-		System.exit(0);
-		});
-		
-		
+		this.initialise();
 	}
 
 	/**
 	 * This method initializes all of the buttons and the layout.
 	 */
 	private void initialiseLayout() {
-		setLayout(null);
+		setLayout(null); // use the absolute layout.
 
 		btnSinglePlayer = new JButton("Single Player");
 		btnSinglePlayer.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnSinglePlayer.setBackground(Color.YELLOW);
-		btnSinglePlayer.setBounds(370, 87, 200, 61);
+		btnSinglePlayer.setBounds(buttonX, 87, buttonWd, buttonHt);
 		add(btnSinglePlayer);
-
+		btnSinglePlayer.addActionListener(e -> {
+			controller.changeView(controller.getMapView()); // change to main												// Menu
+		});
+		
 		btnMultiPlayer = new JButton("Multi Player");
 		btnMultiPlayer.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnMultiPlayer.setBackground(Color.YELLOW);
-		btnMultiPlayer.setBounds(370, 156, 200, 61);
+		btnMultiPlayer.setBounds(buttonX, 156, buttonWd, buttonHt);
 		add(btnMultiPlayer);
+		btnMultiPlayer.addActionListener(e -> {
+		 //TODO : fill me once multiplayer is implemented.
+		});
 
 		btnAbout = new JButton("About \"Missing\"");
 		btnAbout.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnAbout.setBackground(Color.YELLOW);
-		btnAbout.setBounds(370, 224, 200, 61);
+		btnAbout.setBounds(buttonX, 224, buttonWd, buttonHt);
 		add(btnAbout);
+		btnAbout.addActionListener(e -> {
+		JOptionPane.showConfirmDialog(null, creditMessage, "About Missing: The Game", JOptionPane.PLAIN_MESSAGE);
+		});
 
 		btnExit = new JButton("Exit Game");
 		btnExit.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnExit.setBackground(Color.YELLOW);
-		btnExit.setBounds(369, 290, 200, 61);
+		btnExit.setBounds(buttonX, 290, buttonWd, buttonHt);
 		add(btnExit);
+		btnExit.addActionListener(e -> {
+			int option = JOptionPane.showConfirmDialog(null, "Do you want to exit?", "Exit Confirmation",
+					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+			
+			if(option == 0){
+			Runtime.getRuntime().exit(0);
+			}else{
+				return;
+			}
+			
+		});
 
 		btnSinglePlayer = new JButton("Start Single Player");
 		btnMultiPlayer = new JButton("Multi Player");
 		btnAbout = new JButton("About Game");
 		btnExit = new JButton("Exit Game");
-		//this.repaint();
-		this.setupActionListeners();
+		// this.repaint();
 
 	}
 
@@ -113,13 +120,11 @@ public class MenuView extends View {
 		g.setColor(Color.WHITE);
 		g.setFont(new Font("Comic Sans MS", Font.ITALIC, 36));
 		g.drawString("MAIN MENU", (wd / 3), 50);
-		
-		
-		
 	}
 
 	@Override
 	public void initialise() {
+		this.initialiseLayout();
 	}
 
 	@Override
