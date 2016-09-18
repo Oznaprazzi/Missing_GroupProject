4b20a10fa21ec57ef7ac74cfeeec18eefffe7c0f
jianw <jianw@LAPTOP-62IPL4J1>
2016-09-18 23:00:06 +1200

    Added the craftable, unique, tool and machine classes

---
+++ Missing/src/missing/game/items/movable/Craftable.java
@@ -0,0 +1,35 @@
+package missing.game.items.movable;
+
+/*File : Craftable.java
+ * 
+ * Authors    		ID
+ * Jian Wei Chong	300352789
+ * 
+ * Date 		Author		Modification
+ * 18/9/16		Jian Wei	created the class
+ * 
+ * */
+import java.awt.Point;
+/**
+ * This class represents a Craftable item in the Game. It extends the 
+ * Usable, Movable and Item subclasses
+ */
+
+public abstract class Craftable extends Usable{
+
+	/**
+	 * Creates instance of Craftable class.
+	 * 
+	 * @param name
+	 * @param description
+	 * @param worldLocation
+	 * @param tileLocation
+	 * @param amount
+	 * @param size
+	 */
+	public Craftable(String name, String description, Point worldLocation, Point tileLocation, int amount, int size) {
+		super(name, description, worldLocation, tileLocation, amount, size);
+		// TODO Auto-generated constructor stub
+	}
+
+}
