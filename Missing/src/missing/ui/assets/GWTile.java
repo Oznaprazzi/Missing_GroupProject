9d76c32457c552b1b065a81eb02251803a43985f
oznaprazzi <oznaprazzi@OZNAPRAZZI>
2016-09-18 17:32:05 +1200

    Added the draw isometric tiles to the GWTile class

--- Missing/src/missing/ui/assets/GWTile.java
+++ Missing/src/missing/ui/assets/GWTile.java
@@ -9,11 +9,14 @@
  * 	13 Sep 16		Chris Rabe			implemented the draw method
  *  13 Sep 16		Linus Go			added stuff into the draw method.
  *  13 Sep 16		Chris Rabe			made images compatible with executable jar files
+ *  18 Sep 16		Casey Huang			Added the draw isometric tiles method
+ *  18 Sep 16 		Casey Huang			Added integer fields to help draw the isometric tiles
  */
 
 package missing.ui.assets;
 
 import java.awt.Graphics;
+import java.awt.Polygon;
 
 import missing.datastorage.assetloader.GameAssets;
 import missing.game.world.nodes.WorldTile;
@@ -25,7 +28,11 @@ import missing.helper.GameException;
  * single tile in the node.
  */
 public class GWTile {
-
+	public final int TILE_WIDTH = 64; // width of isometric tile
+	public final int TILE_HEIGHT = TILE_WIDTH/2; // height of isometric tile
+	public final int CANVAS_WIDTH = 800;
+	public final int CANVAS_HEIGHT = 500;
+	
 	/** A reference to a tile object. */
 	private WorldTile tile;
 	/** The current size of this tile. */
@@ -74,5 +81,54 @@ public class GWTile {
 		default:
 			throw new GameException("Trying to draw an invalid tile type which doesn't exist!");
 		}
+	}
+	
+	/**
+	 * Draws an individual tile. This is done by drawing an image for each tile and
+	 * clipping it with a polygon which is of the isometric shape. 
+	 * 
+	 * @param g
+	 * @param x x coordinate in grid
+	 * @param y y coordinate in grid
+	 */
+	private void drawIsometricTile(Graphics g, int x, int y){
+		/* Get isometric tile dimensions */
+		// top corner of tile
+		int topX = (x - y) * TILE_WIDTH/2 + CANVAS_WIDTH/2;
+		int topY = (x + y) * TILE_HEIGHT/2;
+		// bottom corner of tile
+		int botX = topX;
+		int botY = topY+TILE_HEIGHT;
+		// left corner of tile
+		int leftX = topX - TILE_WIDTH/2;
+		int leftY = topY + TILE_HEIGHT/2;
+		// right corner of tile
+		int rightX = topX + TILE_WIDTH/2;
+		int rightY = topY + TILE_HEIGHT/2;
+		
+		// Create a polygon to draw tile
+		int[] xPoints = {topX,rightX,botX,leftX};
+		int[] yPoints = {topY,rightY,botY,leftY};
+		Polygon poly = new Polygon(xPoints, yPoints, 4);
+		
+		// Clips the isometric tile shape around the image about to be drawn
+		g.setClip(poly);
+		
+		switch(tile.getType()){
+		case SAND:
+			g.drawImage(GameAssets.getSandImage(), leftX, topY, TILE_WIDTH, TILE_HEIGHT, null);
+			break;
+		case WATER:
+			g.drawImage(GameAssets.getWaterImage(), leftX, topY, TILE_WIDTH, TILE_HEIGHT, null);
+			break;
+		case GRASS:
+			g.drawImage(GameAssets.getGrassImage(), leftX, topY, TILE_WIDTH, TILE_HEIGHT, null);
+			break;
+		case ROAD:
+			g.drawImage(GameAssets.getRoadImage(), leftX, topY, TILE_WIDTH, TILE_HEIGHT, null);
+			break;
+		}
+		// reset clip
+		g.setClip(null);
 	}
 }
