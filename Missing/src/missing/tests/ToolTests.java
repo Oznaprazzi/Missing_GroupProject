e02bb45381f833a10407dc87ce69f9719f0aef72
jianw <jianw@10.140.190.117>
2016-09-22 12:37:15 +1200

    Created a fishing rod and did the initial tests. Created fishArea class, but am confused on what resource to put in the constructor, since fish doesnt extend resource

--- Missing/src/missing/tests/ToolTests.java
+++ Missing/src/missing/tests/ToolTests.java
@@ -6,6 +6,7 @@
  * Date			Author			Modification
  * 19/9/16		Jian Wei		Created initial Axe and PickAxe tests (test1 and invalid test 1)
  * 20/9/16		Jian Wei		Created initial tests for shovel
+ * 22/9/16		Jian Wei		Created initial tests for fishingRod
  * */
 package missing.tests;
 
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
+import missing.game.items.movable.Dirt;
 import missing.game.items.movable.Resource;
 import missing.game.items.movable.Stone;
 import missing.game.items.movable.Tool;
@@ -175,6 +177,56 @@ public class ToolTests {
 		boolean fail = false;
 		try {
 			new Tool(ToolType.SHOVEL, resources, worldLocation, tileLocation);
+		} catch (GameException e) {
+			fail = true;
+		}
+		assertTrue(fail);
+	}
+	
+	/**
+	 * creates a fishing rod, by getting 2 wood and 3 dirt
+	 */
+	@Test
+	public void fishingRodTest_1() {
+		ArrayList<Resource> resources = new ArrayList<Resource>();
+		Point worldLocation = new Point(1, 1);
+		Point tileLocation = new Point(1, 1);
+		for (int i = 0; i < 2; i++) {
+			resources.add(new Wood(worldLocation, tileLocation)); // adds 2 wood
+		}
+		for (int i = 0; i < 3; i++) {
+			resources.add(new Dirt(worldLocation, tileLocation)); // adds 3 dirt
+																	
+		}
+
+		boolean pass = true;
+		try {
+			new Tool(ToolType.FISHINGROD, resources, worldLocation, tileLocation);
+		} catch (GameException e) {
+			pass = false;
+		}
+		assertTrue(pass);
+	}
+
+	/**
+	 * tries to create a fishing rod, but has given 1 wood instead of 2, this should
+	 * throw a game exception
+	 */
+	@Test
+	public void fishingRod_Invalid_Test_1() {
+		ArrayList<Resource> resources = new ArrayList<Resource>();
+		Point worldLocation = new Point(1, 1);
+		Point tileLocation = new Point(1, 1);
+		for (int i = 0; i < 1; i++) {
+			resources.add(new Wood(worldLocation, tileLocation)); // adds 1 wood
+		}
+		for (int i = 0; i < 3; i++) {
+			resources.add(new Dirt(worldLocation, tileLocation)); // adds 3 dirt
+		}
+
+		boolean fail = false;
+		try {
+			new Tool(ToolType.FISHINGROD, resources, worldLocation, tileLocation);
 		} catch (GameException e) {
 			fail = true;
 		}
