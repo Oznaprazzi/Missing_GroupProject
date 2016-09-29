64934a0a3b0ae2c247bfe3265b0af94fc23584db
jianw <jianw@10.140.147.100>
2016-09-30 11:54:25 +1300

    implemented timer for bush, created tests for it

--- Missing/src/missing/game/items/nonmovable/Bush.java
+++ Missing/src/missing/game/items/nonmovable/Bush.java
@@ -11,9 +11,14 @@ package missing.game.items.nonmovable;
 import static java.lang.Math.random;
 
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
 
 import missing.game.characters.Player;
 import missing.game.items.movable.Food;
+import missing.game.items.movable.Wood;
 import missing.game.items.movable.Food.FoodType;
 import missing.helper.GameException;
 
@@ -24,6 +29,9 @@ import missing.helper.GameException;
 public class Bush extends Foliage {
 
 	private static final int BERRY_CHANCE = 70;
+	private Food berry = new Food(worldLocation, tileLocation, FoodType.BERRY);
+	private Timer timer;
+	private static final int REFRESH_TIME_MS = 10000;
 	/**
 	 * Creates a new Bush object at the given locations
 	 * 
@@ -34,15 +42,30 @@ public class Bush extends Foliage {
 	 */
 	public Bush(Point worldLocation, Point tileLocation) {
 		super("Bush", "It's a bush", worldLocation, tileLocation);
+		timer = new Timer(REFRESH_TIME_MS, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if (berry == null) {
+					berry = new Food(worldLocation, tileLocation, FoodType.BERRY);
+				}
+			}
+		});
 	}
 
 	@Override
 	public void performAction(Player player) throws GameException {
+		if(berry == null){
+			throw new GameException("There is no berry in this bush");
+		}
 		int playerChance = (int) (random() * 100);
 		if (playerChance < BERRY_CHANCE) {
-			Food berry = new Food(worldLocation, tileLocation, FoodType.BERRY);
 			berry.setStored(true);
 			player.addToPocket(berry);
+			this.berry = null;
+			// Create a timer which will update resource after n milliseconds
+			if (!timer.isRunning()) {
+				timer.start();
+			}
 		}
 
 	}
