b98ab41f07e4aab24db53e35c981d0976243238c
Christiandel Rabe <chris.rabe14@gmail.com>
2016-10-05 13:04:51 +1300

    made progress on saving

--- Missing/src/missing/datastorage/assetloader/XMLExporter.java
+++ Missing/src/missing/datastorage/assetloader/XMLExporter.java
@@ -8,7 +8,17 @@
 
 package missing.datastorage.assetloader;
 
+import java.awt.Point;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import missing.game.Game;
+import missing.game.world.nodes.WorldNode;
 
 /**
  * This class encapsulates all the saving and exporting logic
@@ -16,6 +26,26 @@ import missing.game.Game;
 public class XMLExporter {
 
 	public static void saveGame(Game game, String filename) {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder builder = dbFactory.newDocumentBuilder();
+			Document doc = builder.newDocument();
+			Element root = doc.createElement("nodes");
+			doc.appendChild(root);
+			WorldNode[][] nodes = game.getWorld().getWorldNodes();
+			for (int y = 0; y < nodes.length; y++) {
+				for (int x = 0; x < nodes[y].length; x++) {
+					writeNodes(new Point(x,y), doc);
+				}
+			}
+		} catch (ParserConfigurationException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+	}
 
+	private static void writeNodes(Point point, Document doc) {
+		// TODO Auto-generated method stub
+		
 	}
 }
