d4c7e9c7d2ff1d0ea2a442f2cd9edac8b9948022
jianw <jianw@10.140.40.133>
2016-10-07 15:35:53 +1300

    created the completeItemsTests class

---
+++ Missing/src/missing/tests/CompleteItemsTests.java
@@ -0,0 +1,23 @@
+/*File: CompleteItemsTests.java
+ * 
+ * Authors				ID
+ * Jian Wei Chong		300352789
+ * 
+ * Date					Author			Modification
+ * 7 oct 16				Jian Wei		Created the class*/
+package missing.tests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * This class runs all the items tests*/
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		SourceTests.class,
+		FurnitureTests.class,
+		ToolTests.class,
+		SourceTimerTests.class
+})
+public class CompleteItemsTests {
+
+}
