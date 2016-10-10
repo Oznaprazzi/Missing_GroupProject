1aaf6f2222c1352b220301a87805792e2e56e9f4
Christiandel Rabe <chris.rabe14@gmail.com>
2016-10-11 12:53:40 +1300

    restructured VControl

--- Missing/src/missing/ui/frames/HandJFrame.java
+++ Missing/src/missing/ui/frames/HandJFrame.java
@@ -20,7 +20,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import missing.datastorage.assetloader.GameAssets;
-import missing.game.characters.Player;
 import missing.game.items.movable.Movable;
 import missing.game.items.nonmovable.Bag;
 import missing.game.items.nonmovable.Pocket;
@@ -30,35 +29,36 @@ import missing.ui.menustyle.MenuFactory;
 
 @SuppressWarnings("serial")
 public class HandJFrame extends JFrame {
-	/*Height and Width of this JFrame */
+	/* Height and Width of this JFrame */
 	private final int FRAME_WD = 434;
 	private final int FRAME_HT = 481;
-	/*The content Pane */
+	/* The content Pane */
 	private ImagePanel contentPane;
-	
-	/*The current background Image */
+
+	/* The current background Image */
 	private BufferedImage backgroundImage = GameAssets.getWindowBackgroundImage();
-	
-	/*Buttons */
+
+	/* Buttons */
 	private final JButton btnBagToPocket = MenuFactory.createButton2("Transfer To Pocket");
 	private final JButton btnPocketToBag = MenuFactory.createButton2("Transfer To Bag");
 	private final JButton btnUseItem = MenuFactory.createButton2("Use Item");
 	private final JButton btnDrop = MenuFactory.createButton2("Drop");
-	
-	/**The hand canvas */
-	private HandPanel panel; //the hand canvas
-	
-	/*The bag and pocket */
+
+	/** The hand canvas */
+	private HandPanel panel; // the hand canvas
+
+	/* The bag and pocket */
 	private Bag bag;
 	private Pocket pocket;
-	
-	
+
+	private VControl control;
+
 	/**
 	 * Create a Test canvas contained within this window.
 	 */
 	public HandJFrame(VControl control) {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		//setSize(backgroundImage.getWidth(), backgroundImage.getHeight());
+		// setSize(backgroundImage.getWidth(), backgroundImage.getHeight());
 		setSize(FRAME_WD, FRAME_HT);
 		// elsewhere
 		contentPane = new ImagePanel();
@@ -69,48 +69,50 @@ public class HandJFrame extends JFrame {
 		contentPane.add(btnDrop);
 		panel = new HandPanel(control);
 		contentPane.add(panel);
+		this.control = control;
 		addActionListeners();
 	}
-	
+
 	private class ImagePanel extends JPanel {
-	    @Override
-	    protected void paintComponent(Graphics g) {
-	        super.paintComponent(g);
-	        g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
-	    }
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+		}
 	}
 
 	/**
 	 * Sets up the Action Listeners for the buttons.
 	 */
 	private void addActionListeners() {
-		btnPocketToBag.addActionListener(e->{
+		btnPocketToBag.addActionListener(e -> {
 			try {
 				panel.transferPocketToBag();
 			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
 		});
-		btnBagToPocket.addActionListener(e->{
+		btnBagToPocket.addActionListener(e -> {
 			try {
 				panel.transferBagToPocket();
 			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
 		});
-		/*Called when the item is used. */
-		btnUseItem.addActionListener(e->{
-			try{
+		/* Called when the item is used. */
+		btnUseItem.addActionListener(e -> {
+			try {
 				panel.useItem();
-			}catch(Exception e1){
+			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
 		});
-		btnDrop.addActionListener(e->{
-			//TODO: implement Chris.
-			//Item dropping logic would go inside of here.
+		btnDrop.addActionListener(e -> {
+			Movable item = panel.getSelectedItem();
+			if (item != null) {
+			}
 		});
-		
+
 	}
-	
+
 }
